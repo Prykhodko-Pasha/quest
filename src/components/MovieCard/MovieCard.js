6978b16d1dc59e40596a91ce99d6a043a3c45616
Prykhodko-Pasha <prykhodko.p@gmail.com>
2022-01-15 11:06:29 +0200

    mvp

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,6 +7,7 @@ import questionsArr from '../questions.js';
 export default function MovieCard({ movie }) {
   let [number, setNumber] = useState(0);
   const [showModal, setshowModal] = useState(false);
+  const [showEnd, setshowEnd] = useState(false);
   const [inputText, setInputText] = useState('');
 
   const onChange = e => {
@@ -19,7 +20,8 @@ export default function MovieCard({ movie }) {
         'questionsArr[number].answer :>> ',
         questionsArr[number].answer,
       );
-      setNumber((number += 1));
+      if (number === questionsArr.length - 1) setshowEnd(true);
+      else setNumber((number += 1));
     } else {
       setshowModal(!showModal);
     }
@@ -62,12 +64,35 @@ export default function MovieCard({ movie }) {
         <Popup open={showModal} modal closeOnDocumentClick>
           {close => (
             <div className={s.modal}>
-              <button className={s.close} onClick={close}>
+              <button className={s.closeSign} onClick={close}>
                 &times;
               </button>
-              <div className={s.header}> Sorry </div>
-              <div className={s.content}>This is wrong code...</div>
-              <div className={s.actions}>
+              <div className={s.modal__header}> Sorry </div>
+              <div className={s.modal__content}>This is wrong code...</div>
+              <div className={s.modal__actions}>
+                <button
+                  className={s.form__btn}
+                  onClick={() => {
+                    console.log('modal closed ');
+                    close();
+                  }}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
+        </Popup>
+
+        <Popup open={showEnd} modal closeOnDocumentClick>
+          {close => (
+            <div className={s.modalWin}>
+              <button className={s.closeSign} onClick={close}>
+                &times;
+              </button>
+              <div className={s.modal__header}> Ура! </div>
+              <div className={s.modal__content}>Ви це зробили :)</div>
+              <div className={s.modal__actions}>
                 <button
                   className={s.form__btn}
                   onClick={() => {
